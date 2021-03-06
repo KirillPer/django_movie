ed4b58458d795695608322a8bf27b69dc9d48e61
KirillPer <asmodeyold420@gmail.com>
2021-03-06 20:33:59 +0700

    Добавлены отзывы

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
-
+from .forms import ReviewForm
 # Create your views here.
 
 
-class MoviesView(View):
+class MoviesView(ListView):
 
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {'movie_list': movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    #template_name = 'movies/movie_list.html'
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = 'url'
+
+
+class AddReview(View):
+
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
